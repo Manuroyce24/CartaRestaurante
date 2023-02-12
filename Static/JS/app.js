a871bbc1e8321d627b8ac6946959074c2e12fd92
Manuroyce24 <95409974+Manuroyce24@users.noreply.github.com>
2023-02-12 15:25:05 +0100

    Versión final

---
+++ Static/JS/app.js
@@ -0,0 +1,192 @@
+let template = document.querySelector("#plantilla-plato")
+let menu = document.querySelector("section#platos div")
+let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+
+fetch('https://63d011d710982404378c822f.mockapi.io/carta')
+  .then(response => response.json())
+  .then(data => {
+
+    carta = data;
+    mostrarCarta();
+    const botones = document.querySelectorAll("button");
+    botones.forEach(boton => {
+      boton.addEventListener("click", function () {
+
+        // if (this.checked) {
+        let platito = this.closest("div");
+
+        let platitoFavorito = {
+          image: platito.querySelector("img").src,
+          plato: platito.querySelector("h4").innerHTML,
+          ingredientes: platito.querySelector(".ingredientes").innerHTML,
+          precio: platito.querySelector(".precio").innerHTML,
+          alergenos: platito.querySelector(".alergenos").innerHTML
+        };
+        favoritos.push(platitoFavorito);
+        localStorage.setItem("favoritos", JSON.stringify(favoritos));
+      });
+    });
+
+    let mostrarFav = false;
+    document.querySelector("#mostrarFavoritos").addEventListener("click", function () {
+      mostrarFav = !mostrarFav;
+
+      if (mostrarFav) {
+        // Recuperar los favoritos del local storage
+        let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+
+        // Recorrer los favoritos y crear las filas de la tabla
+        favoritos.forEach(function (plato) {
+
+          // Construir el template
+          const row = document.createElement('tr');
+          row.innerHTML = `<td> 
+                <img src="${plato.image}" width=100> 
+               </td>
+               <td>${plato.plato}</td>
+   
+               <td>
+                <a href="#" class="borrar-curso" data-id="${plato.id}">❌</a>
+               </td>`;
+          document.querySelector('#lista-carrito tbody').appendChild(row);
+        });
+
+        // Event listener para borrar
+        document.querySelector("tbody").addEventListener("click", function (e) {
+          if (e.target.classList.contains("borrar-curso")) {
+            e.target.parentElement.parentElement.remove();
+
+            // Borra de favoritos
+            const id = e.target.getAttribute("data-id");
+            const index = favoritos.findIndex(plato => plato.id === id);
+            favoritos.splice(index, 1);
+
+            localStorage.setItem("favoritos", JSON.stringify(favoritos));
+          }
+        });
+      } else {
+        // Limpiar la tabla
+        document.querySelector("tbody").innerHTML = "";
+      }
+    });
+
+    function obtenerPlatosLocalStorage() {
+    }
+  });
+
+
+//Mostrar carta con filtros
+function mostrarCarta() {
+  const celiaco = document.getElementById("celiaco").checked;
+  const sinLactosa = document.getElementById("sinlactosa").checked;
+  const vegetariano = document.getElementById("vegetariano").checked;
+
+  const platos = carta.filter(plato => {
+
+    if (celiaco && !plato.celiaco) return false;
+    if (sinLactosa && !plato.sinlactosa) return false;
+    if (vegetariano && !plato.vegetariano) return false;
+
+    return true;
+  });
+
+  const menu = document.querySelector("section#platos div");
+  // console.log(menu)
+  menu.innerHTML = "";
+  platos.forEach(plato => {
+    const clon = document.importNode(template.content, true);
+    clon.querySelector("img").src = plato.image;
+    clon.querySelector("h4").innerHTML = plato.plato;
+    clon.querySelector(".ingredientes").innerHTML = plato.ingredientes;
+    clon.querySelector(".precio").innerHTML = plato.precio;
+    clon.querySelector(".alergenos").innerHTML = plato.alergenos;
+    menu.appendChild(clon);
+  });
+}
+
+
+function mostrarFavoritos() {
+
+  let menu = document.querySelector("section#platos div");
+  if (!mostrarFav) {
+
+    let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+    menu.innerHTML = "";
+    // Pintamos favoritos en la pantalla
+
+    favoritos.forEach(plato => {
+
+      let plantilla = template.content.cloneNode(true);
+      plantilla.querySelector("img").src = plato.image;
+      plantilla.querySelector("h4").innerHTML = plato.plato;
+      plantilla.querySelector(".ingredientes").innerHTML = plato.ingredientes;
+      plantilla.querySelector(".precio").innerHTML = plato.precio;
+      plantilla.querySelector(".alergenos").innerHTML = plato.alergenos;
+      plantilla.querySelector("input[type='checkbox']").style.display = "hidden";
+      plantilla.querySelector("#ocultable").style.display = "none";
+      menu.appendChild(plantilla);
+    });
+
+    mostrarFav = true;
+  } else {
+    mostrarCarta();
+    mostrarFav = false;
+  }
+}
+
+function borrarFavoritos() {
+  favoritos = []
+  localStorage.setItem("favoritos", JSON.stringify(favoritos));
+}
+
+//Filtro por precio
+
+let order = 'desc';
+
+document.querySelector('#sort').addEventListener('click', function () {
+  if (order === 'desc') {
+    order = 'asc';
+  } else {
+    order = 'desc';
+  }
+
+  let menu = document.querySelector("section#platos div");
+  let items = Array.from(menu.children);
+
+  items.sort(function (a, b) {
+    let aPrice = parseInt(a.querySelector('p.precio').textContent);
+    let bPrice = parseInt(b.querySelector('p.precio').textContent);
+
+    if (order === 'asc') {
+      return aPrice - bPrice;
+    } else {
+      return bPrice - aPrice;
+    }
+  });
+
+  menu.innerHTML = '';
+  menu.append(...items);
+});
+
+
+var store = window.localStorage;
+document.getElementById("filter").addEventListener("click", mostrarCarta);
+
+function initStorage() {
+
+  if (store.getItem("favoritos") == null) {
+    store.setItem("favoritos", JSON.stringify(favoritos));
+  }
+  if (store.getItem("cursor") == null) {
+    store.setItem("cursor", 0);
+  }
+  favoritos = JSON.parse(store.getItem("favoritos"));
+  cursor = store.getItem("cursor")
+
+  console.table(favoritos)
+
+}
+window.addEventListener('load', (event) => {
+  initStorage()
+
+});
